a10b2f4c9a5dfdb08fd13f9effe7a1a36d1dfa24
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-12 12:35:07 +0100

    Second Test Complete

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -8,4 +8,10 @@ def get_pet_shop_name(name):
     return name["name"]
 
 
+# @unittest.skip("delete this line to run the test")
+# def test_total_cash(self):
+#     sum = get_total_cash(self.cc_pet_shop)
+#     self.assertEqual(1000, sum)
 
+def get_total_cash(sum):
+    return sum["admin"]["total_cash"]
