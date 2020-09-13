fe5b3191806176002d7ab6980714cd7b879e4be7
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-13 21:04:27 +0100

    test 12 done with some of test 13 in place - NOT YET WORKING

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -23,27 +23,22 @@ def get_stock_count(pet_shop):
     #     pet_count += 1
     # return pet_count    
 
-
-        # def test_all_pets_by_breed__found(self):
-        #     pets = get_pets_by_breed(self.cc_pet_shop, "British Shorthair")
-        #     self.assertEqual(2, len(pets))
-
-            # def get_pets_by_breed(pet_shop, breed):
-            #     pet_by_breed = []
-            #     for pet in pet_shop["pets"]:
-            #         if pet["breed"] == breed:
-            #             pet_by_breed.append(pet)
-            #         return(pet_by_breed)
-
-        # @unittest.skip("delete this line to run the test")
-        # def test_all_pets_by_breed__not_found(self):
-        #     pets = get_pets_by_breed(self.cc_pet_shop, "Dalmation")
-        #     self.assertEqual(0, len(pets))
+def get_pets_by_breed(pet_shop, breed):
+    pet_by_breed = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pet_by_breed.append(breed)
+    return pet_by_breed
 
 def find_pet_by_name(pet_shop, pet_name):
     for pet in pet_shop["pets"]:
         if pet["name"] == pet_name:
-            return(pet)
+            return pet
+
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet            
 
         # @unittest.skip("delete this line to run the test")
         # def test_remove_pet_by_name(self):
@@ -55,9 +50,29 @@ def find_pet_by_name(pet_shop, pet_name):
             #     for pet in pet_shop["pets"]:
             #         if pet["name"] == pet_name:
             #             del(pet)
-            #             return(pet)
+            #     return(pet)
+            
+    # @unittest.skip("delete this line to run the test")
+    # def test_add_pet_to_stock(self):
+    #     add_pet_to_stock(self.cc_pet_shop, self.new_pet)
+    #     count = get_stock_count(self.cc_pet_shop)
+    #     self.assertEqual(7, count)
 
+# def add_pet_to_stock(pet_shop, new_pet):
+#     pet_shop["pets"]
 
-def add_pet_to_stock(pet_shop, new_pet):
-    pet_shop["pets"]
+def get_customer_cash(customer):
+    return customer["cash"]
 
+    # @unittest.skip("delete this line to run the test")
+    # def test_remove_customer_cash(self):
+    #     customer = self.customers[0]
+    #     remove_customer_cash(customer, 100)
+    #     self.assertEqual(900, customer["cash"])
+
+def remove_customer_cash(customer, transaction_amount):
+    customer -= transaction_amount
+    return customer
+
+# def add_or_remove_cash(pet_shop,transaction_amount):
+#     pet_shop["admin"]["total_cash"] += transaction_amount
--- tests/pet_shop_test.py
+++ tests/pet_shop_test.py
@@ -106,12 +106,10 @@ class TestPetShop(unittest.TestCase):
         count = get_stock_count(self.cc_pet_shop)
         self.assertEqual(6, count)
 
-    @unittest.skip("delete this line to run the test")
     def test_all_pets_by_breed__found(self):
         pets = get_pets_by_breed(self.cc_pet_shop, "British Shorthair")
         self.assertEqual(2, len(pets))
 
-    @unittest.skip("delete this line to run the test")
     def test_all_pets_by_breed__not_found(self):
         pets = get_pets_by_breed(self.cc_pet_shop, "Dalmation")
         self.assertEqual(0, len(pets))
@@ -136,12 +134,11 @@ class TestPetShop(unittest.TestCase):
         count = get_stock_count(self.cc_pet_shop)
         self.assertEqual(7, count)
 
-    @unittest.skip("delete this line to run the test")
+
     def test_customer_cash(self):
         cash = get_customer_cash(self.customers[0])
         self.assertEqual(1000, cash)
 
-    @unittest.skip("delete this line to run the test")
     def test_remove_customer_cash(self):
         customer = self.customers[0]
         remove_customer_cash(customer, 100)
